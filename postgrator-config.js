40d10115e83cf6d7bd077d91ef0091181f80c02f
Caleb Jackson <jacksoncaleb.calebjackson@gmail.com>
2020-11-10 18:24:40 -0500

    db

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,10 +3,6 @@ require('dotenv').config();
 module.exports = {
   'migrationDirectory': 'migrations',
   'driver': 'pg',
-  // "connection":
-  //   process.env.NODE_ENV === "test"
-  //     ? process.env.TEST_DATABASE_URL
-  //     : process.env.DATABASE_URL,
   'connectionString': process.env.NODE_ENV === 'test'
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
@@ -16,14 +12,3 @@ module.exports = {
   'username': process.env.MIGRATION_DATABASE_USER,
   'password': process.env.MIGRATION_DATABASE_PASS
 };
-
-
-
-// module.exports = {
-//   migrationsDirectory: "migrations",
-//   driver: "pg",
-//   connection:
-//     process.env.NODE_ENV === "test"
-//       ? process.env.TEST_DATABASE_URL
-//       : process.env.DATABASE_URL
-// };
