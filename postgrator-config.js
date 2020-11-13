ba1c5c2a75af89391bb58778c303ce0c08a7caf1
Caleb Jackson <jacksoncaleb.calebjackson@gmail.com>
2020-11-13 12:23:14 -0500

    updated postgrator-config

--- postgrator-config.js
+++ postgrator-config.js
@@ -6,9 +6,18 @@ module.exports = {
   'connectionString': process.env.NODE_ENV === 'test'
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
-  'host': process.env.MIGRATION_DATABASE_HOST,
-  'port': process.env.MIGRATION_DATABASE_PORT,
-  'database': process.env.MIGRATION_DATABASE_NAME,
-  'username': process.env.MIGRATION_DATABASE_USER,
-  'password': process.env.MIGRATION_DATABASE_PASS
+  'host': process.env.PROD_MIGRATION_DATABASE_HOST,
+  'port': process.env.PROD_MIGRATION_DATABASE_PORT,
+  'database': process.env.PROD_MIGRATION_DATABASE_NAME,
+  'username': process.env.PROD_MIGRATION_DATABASE_USER,
+  'password': process.env.PROD_MIGRATION_DATABASE_PASS
 };
+
+//when testing locally, basically just remove prod_
+//also dft change the db url in .env
+//"postgresql://dunder_mifflin@localhost/spaced-repetition"
+// 'host': process.env.MIGRATION_DATABASE_HOST,
+//   'port': process.env.MIGRATION_DATABASE_PORT,
+//     'database': process.env.MIGRATION_DATABASE_NAME,
+//       'username': process.env.MIGRATION_DATABASE_USER,
+//         'password': process.env.MIGRATION_DATABASE_PASS
