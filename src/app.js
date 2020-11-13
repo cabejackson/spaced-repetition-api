8124880e1fa9be2088772fb7a1e83ce55a321c5a
Caleb Jackson <jacksoncaleb.calebjackson@gmail.com>
2020-11-13 11:20:00 -0500

    cors update

--- src/app.js
+++ src/app.js
@@ -9,12 +9,13 @@ const languageRouter = require('./language/language-router');
 const userRouter = require('./user/user-router');
 
 const app = express();
-
+app.use(cors());
+app.use(helmet());
 app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
   skip: () => NODE_ENV === 'test',
 }));
-app.use(helmet());
-app.use(cors());
+
+
 
 app.use('/api/auth', authRouter);
 app.use('/api/language', languageRouter);
